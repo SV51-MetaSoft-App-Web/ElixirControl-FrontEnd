aec471b4dae50e2e04b12112153338e826fc6f7c
jhordi <u20191e835@upc.edu.pe>
2024-09-25 22:48:20 -0500

    feat(security): added content of "index.js".

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/views/HomePage.vue';
+import LoginDistributor from '@/views/LoginDistributor.vue';
+import LoginProducer from '@/views/LoginProducer.vue';
+import RegisterProducer from '@/views/RegisterProducer.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomePage,
+    },
+    {
+        path: '/login-distributor',
+        name: 'LoginDistributor',
+        component: LoginDistributor,
+    },
+    {
+        path: '/login-producer',
+        name: 'LoginProducer',
+        component: LoginProducer,
+    },
+    {
+        path: '/register-producer',
+        name: 'RegisterProducer',
+        component: RegisterProducer,
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [{}]
+    routes,
 });
 
-
-
 export default router;
+
+
