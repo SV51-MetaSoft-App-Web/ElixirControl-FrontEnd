cb0a0b2c9e4095d8d52cabb7915c047a5744c78f
jhordi <u20191e835@upc.edu.pe>
2024-09-25 22:53:30 -0500

    feat(security): added content of "i18n.js" and "style.css".

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,16 @@
+import { createI18n } from 'vue-i18n';
 import en from './locales/en.json';
 import es from './locales/es.json';
 
-import {createI18n} from "vue-i18n";
+const messages = {
+    en,
+    es
+};
 
-const i18n=createI18n(
-    {
-        legacy:false,
-        locale:'en',
-        globalInjection:true,
-        messages:{
-            en,
-            es
-        }
-    }
-)
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages,
+});
 
 export default i18n;
