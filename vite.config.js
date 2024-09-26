6810bd939a24f60aabba0049ea5e1642a265c5e9
jhordi <u20191e835@upc.edu.pe>
2024-09-25 22:54:49 -0500

    feat(security): added content of "vite.config.js".

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    hmr: {
+      overlay: true, // Asegúrate que esto esté habilitado
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // Alias @ apunta a la carpeta src
+    }
+  }
 })
