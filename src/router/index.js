1d54c1e4abf1cce5b308b06237c18bd96f9da69c
petitavo <Gustavohuanca1020@gmail.com>
2024-10-16 18:40:08 -0500

    feat(customer-management): client details and management routes added.

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import batchManagementComponent from "../elixir-control/winemaking-process/pages/batch-management.component.vue";
-import fermentationManagementComponent from "../elixir-control/winemaking-process/pages/fermentation-management.component.vue";
-import agingManagementComponent from "../elixir-control/winemaking-process/pages/aging-management.component.vue";
-import BottlingManagementComponent from "../elixir-control/winemaking-process/pages/bottling-management.component.vue";
-import ClarificationManagementComponent
-    from "../elixir-control/winemaking-process/pages/clarification-management.component.vue";
-import PressingManagementComponent from "../elixir-control/winemaking-process/pages/pressing-management.component.vue";
-import WinemakingProcessManagementComponent
-    from "../elixir-control/winemaking-process/pages/winemaking-process-management.component.vue";
-import ProductManagementComponent from "../elixir-control/products-mangment/pages/product-management.component.vue";
-import InventoryManagementComponent from "../elixir-control/inventory-management/pages/inventory-management.vue";
-import OrderHistory from "../elixir-control/distributor-profile/pages/order-history.vue";
 import HomeContentComponent from "../public/pages/home-content.component.vue";
+import ClientManagementComponent from "../elixir-control/customer-management/pages/client-management.component.vue";
+import ClientDetailsComponent from "../elixir-control/customer-management/pages/client-details.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/productor/winemaking-process/batches',          name: 'batches',            component: batchManagementComponent,            meta: { title: 'Batches'}},
-        {path: '/productor/winemaking-process/fermentation',     name: 'Fermentation',       component: fermentationManagementComponent,     meta: { title: 'Fermentation'}},
-        {path: '/productor/winemaking-process/aging',            name: 'Aging',              component: agingManagementComponent,            meta: { title: 'Aging'}},
-        {path: '/productor/winemaking-process/bottling',         name: 'Bottling',           component: BottlingManagementComponent,         meta: { title: 'Bottling'}},
-        {path: '/productor/winemaking-process/clarification',    name: 'Clarification',      component: ClarificationManagementComponent,    meta: { title: 'Clarification'}},
-        {path: '/productor/winemaking-process/pressing',         name: 'Pressing',           component: PressingManagementComponent,         meta: { title: 'Pressing'}},
-        {path: '/productor/winemaking-process',                  name: 'Winemaking Process', component: WinemakingProcessManagementComponent,meta: { title: 'Winemaking Process'}},
-        {path: '/productor/inventory',                           name: 'InventoryManagement',component: InventoryManagementComponent,        meta: { title: 'Inventory Management' }},
-        {path: '/productor/orders/history',                      name: 'OrderHistory',       component: OrderHistory,                        meta: { title: 'Order History' }},
-        {path: '/productor/products',                            name: 'Products',           component: ProductManagementComponent,          meta: { title: 'Products'}},
-        {path: '/home',                                         name: 'Home',                component: HomeContentComponent,                meta: { title: 'Home'}},
+        {
+            path: '/',
+            name: 'Home',
+            component: HomeContentComponent,
+            meta: { title: 'Home'}
+        },
+        {
+            path:'/productor/clients',
+            name: 'Clients',
+            component: ClientManagementComponent,
+            meta: { title: 'Clients'}
+        },
+        {
+            path:'/productor/clients/details/:id',
+            name: 'ClientDetails',
+            component: ClientDetailsComponent,
+            meta: { title: 'Client Details'},
+            props: true
+        }
     ]
 });
-
-
-
 
 export default router;
