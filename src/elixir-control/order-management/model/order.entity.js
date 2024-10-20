062789d25ef8e245ca94e7e1927b40bed4e67013
vquijandria <u201822697@upc.edu.pe>
2024-10-19 20:46:40 -0500

    refactor(order.entity):  edited the order entity

--- src/elixir-control/order-management/model/order.entity.js
+++ src/elixir-control/order-management/model/order.entity.js
@@ -1,34 +1,29 @@
-import { Product } from './product.entity';
-export class Order extends  Product {
+export class Order {
     constructor(
         id,
-        businessName,
-        contactName,
-        email,
-        address,
-        ruc,
+        business,
+        date,
         quantity,
         phone,
-        name,
-        paymentMethod,
-        paymentTerms,
-        date,
-        deliveryDate,
-        orderStatus
+        contact
     ) {
-        super(name);
-        this.id = id; // Identificador único del pedido / nro solicitud
-        this.businessName = businessName; // Nombre del negocio
-        this.contactName = contactName; // Nombre del contacto
-        this.email = email; // Correo electrónico del contacto
-        this.address = address; // Dirección del contacto
-        this.ruc = ruc; // RUC del negocio o contacto
-        this.quantity = quantity; // Cantidad de productos
-        this.phone = phone; // Teléfono del contacto
-        this.paymentMethod = paymentMethod; // Metodo de pago utilizado
-        this.paymentTerms = paymentTerms; // Plazo de pago
-        this.date = date; // Fecha del pedido
-        this.deliveryDate = deliveryDate; // Fecha de entrega
-        this.orderStatus = orderStatus; // Estado del pedido
+        this.id = id;
+        this.business = business;
+        this.date = date;
+        this.quantity = quantity;
+        this.phone = phone;
+        this.contact = contact;
+    }
+}
+
+export class Contact {
+    constructor(phone, address, email, ruc, wineType, paymentMethod, deliveryDate) {
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.ruc = ruc;
+        this.wineType = wineType;
+        this.paymentMethod = paymentMethod;
+        this.deliveryDate = deliveryDate;
     }
 }
